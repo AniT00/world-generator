7fbac95eb2e6d15c780a5141d1c424ef28b8d3f1
Yan Perepelitsyn <yan.perepelitsyn@gmail.com>
2023-06-28 15:10:49 +0300

    perlinNoise done

--- world-generator/main.cpp
+++ world-generator/main.cpp
@@ -3,10 +3,16 @@
 #include <iostream>
 #include <random>
 #include <assert.h>
+#include "main.h"
 
 const int WINDOW_WIDTH	= 1280;
 const int WINDOW_HEIGHT = 720;
-const size_t GRID_CELL_SIZE = 20;
+const size_t GRID_CELL_SIZE = 30;
+
+void generateMap(float* map, size_t width, size_t height, size_t grid_cell_size);
+
+void mapToPixels(float* map, size_t width, size_t height, sf::Uint8* pixels, size_t p_width = WINDOW_WIDTH);
+
 
 template<typename T>
 inline float dotProduct(const sf::Vector2<T>& lh, const sf::Vector2<T>& rh) {
@@ -14,8 +20,8 @@ inline float dotProduct(const sf::Vector2<T>& lh, const sf::Vector2<T>& rh) {
 }
 
 float smoothstep(float x) {
-	//return (6 * x * x * x * x * x - 15 * x * x * x * x + 10 * x * x * x);
-	return (3.0 - x * 2.0) * x * x;
+	return (6 * x * x * x * x * x - 15 * x * x * x * x + 10 * x * x * x);
+	//return (3.0 - x * 2.0) * x * x;
 	//return x;
 	//return ((x * (x * 6.0 - 15.0) + 10.0) * x * x * x);
 }
@@ -26,7 +32,7 @@ float interpolate(float a, float b, float w) {
 	return a + smoothstep(w) * (b - a);
 }
 
-void f(float* map, size_t width, size_t height, size_t grid_cell_size) {
+void perlinNoise(float* map, size_t width, size_t height, size_t grid_cell_size) {
 	assert(width % grid_cell_size == 0 && height % grid_cell_size == 0);
 	int grid_w = width  / grid_cell_size + 1;
 	int grid_h = height / grid_cell_size + 1;
@@ -56,11 +62,6 @@ void f(float* map, size_t width, size_t height, size_t grid_cell_size) {
 			sf::Vector2f* top_right		= grid + (i / grid_cell_size * grid_w + j / grid_cell_size + 1);
 			sf::Vector2f* bottom_left	= grid + ((i / grid_cell_size + 1) * grid_w + j / grid_cell_size);
 			sf::Vector2f* bottom_right	= grid + ((i / grid_cell_size + 1) * grid_w + j / grid_cell_size + 1);
-
-			//sf::Vector2f top_left_offset	 = (point / (float)grid_cell_size - sf::Vector2f((float)j / grid_cell_size,		(float)i / grid_cell_size))		;// (float)grid_cell_size;
-			//sf::Vector2f top_right_offset	 = (point / (float)grid_cell_size - sf::Vector2f((float)j / grid_cell_size + 1,	(float)i / grid_cell_size))		;// (float)grid_cell_size;
-			//sf::Vector2f bottom_left_offset	 = (point / (float)grid_cell_size - sf::Vector2f((float)j / grid_cell_size,		(float)i / grid_cell_size + 1)) ;// (float)grid_cell_size;
-			//sf::Vector2f bottom_right_offset = (point / (float)grid_cell_size - sf::Vector2f((float)j / grid_cell_size + 1,	(float)i / grid_cell_size + 1)) ;// (float)grid_cell_size;
 
 			sf::Vector2f relative_point = point - sf::Vector2f(j / grid_cell_size * grid_cell_size, i / grid_cell_size * grid_cell_size);
 			sf::Vector2f top_left_offset	 = relative_point / (float)grid_cell_size;
@@ -96,6 +97,7 @@ size_t autoHeight(size_t max = WINDOW_HEIGHT) {
 int main(int argc, char** argv) {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "wg");
 	window.setKeyRepeatEnabled(false);
+	window.setFramerateLimit(60);
 
 	float* map = new float[WINDOW_WIDTH * WINDOW_HEIGHT] { 0 };
 	sf::Uint8* pixels = new sf::Uint8[WINDOW_WIDTH * WINDOW_HEIGHT * 4]{ 0 };
@@ -109,13 +111,18 @@ int main(int argc, char** argv) {
 
 	const size_t map_height = autoHeight();
 	const size_t map_width = autoWidth();
-	//f(map, map_width, map_height, GRID_CELL_SIZE);
 	
 	sf::Texture mapTex;
 	mapTex.create(WINDOW_WIDTH, WINDOW_HEIGHT);
 	sf::Sprite s(mapTex);
 	float scale_factor = std::min(WINDOW_WIDTH / map_width, WINDOW_HEIGHT / map_height);
 	s.setScale(scale_factor, scale_factor);
+
+	generateMap(map, map_width, map_height, GRID_CELL_SIZE);
+	mapToPixels(map, map_width, map_height, pixels);
+	mapTex.update(pixels);
+	window.draw(s);
+	window.display();
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -124,35 +131,37 @@ int main(int argc, char** argv) {
 			}
 			if (event.type == event.KeyPressed) {
 				if (event.key.code == sf::Keyboard::D) {
-					window.clear(sf::Color::White);
-					f(map, map_width, map_height, GRID_CELL_SIZE);
-					system("cls");
-					for (size_t i = 0; i < map_height; i++)
-					{
-						sf::Uint8 color = 0.f;
-						for (size_t j = 0; j < map_width; j++)
-						{
-							/*if (i % GRID_CELL_SIZE == 0 || j % GRID_CELL_SIZE == 0) {
-								pixels[(i * WINDOW_WIDTH + j) * 4] = 255;
-								pixels[(i * WINDOW_WIDTH + j) * 4 + 1] = 255;
-								pixels[(i * WINDOW_WIDTH + j) * 4 + 2] = 255;
-								continue;
-							}*/
-							color = map[i * map_width + j] * 10 ;
-							pixels[(i * WINDOW_WIDTH + j) * 4]	   = color;
-							pixels[(i * WINDOW_WIDTH + j) * 4 + 1] = color;
-							pixels[(i * WINDOW_WIDTH + j) * 4 + 2] = color;
-						}
-						std::cout << color << "\n";
-					}
+					generateMap(map, map_width, map_height, GRID_CELL_SIZE);
+					mapToPixels(map, map_width, map_height, pixels);
 					mapTex.update(pixels);
-					window.draw(s);
-					window.display();
+					
 				}
 			}
 		}
-		
-		//std::cin.get();
+		window.clear(sf::Color::White);
+
+		window.draw(s);
+		window.display();
 	}
 	return 0;
+}
+
+void generateMap(float* map, size_t width, size_t height, size_t grid_cell_size)
+{
+	perlinNoise(map, width, height, grid_cell_size);
+	
+}
+
+void mapToPixels(float* map, size_t width, size_t height, sf::Uint8* pixels, size_t p_width)
+{
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+			float color = ((map[i * width + j] + 1.f) * 0.5) * 255;
+			pixels[(i * p_width + j) * 4]	  = color;
+			pixels[(i * p_width + j) * 4 + 1] = color;
+			pixels[(i * p_width + j) * 4 + 2] = color;
+		}
+	}
 }
